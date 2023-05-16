Fixed calculation max flux

actsnfink/classifier_sigmoid.py
@@ -316,7 +316,7 @@ def get_max_fluxcal(data, list_filters):
         data_tmp = filter_data(data, i)
         local_max_fluxcal.append(data_tmp['FLUXCAL'].max())
 
-    max_fluxcal = np.max(local_max_fluxcal)
+    max_fluxcal = np.nanmax(local_max_fluxcal)
 
     return max_fluxcal
 